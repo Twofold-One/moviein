4ec617fb9c351e0b7dc7f6daa77bebfd1814623c
Twofold-One <evan_geras@mail.ru>
2022-10-25 23:48:56 +0700

    users migrations and model added

--- go.mod
+++ go.mod
@@ -7,3 +7,5 @@ require github.com/julienschmidt/httprouter v1.3.0
 require github.com/lib/pq v1.10.7
 
 require golang.org/x/time v0.0.0-20220920022843-2ce7c2934d45
+
+require golang.org/x/crypto v0.1.0
---
+++ internal/data/users.go
@@ -0,0 +1,49 @@
+package data
+
+import (
+	"errors"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"-"`
+}
+
+type password struct {
+	plaintext *string
+	hash      []byte
+}
+
+func (p *password) Set(plaintextPassword string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(plaintextPassword), 12)
+	if err != nil {
+		return err
+	}
+
+	p.plaintext = &plaintextPassword
+	p.hash = hash
+
+	return nil
+}
+
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+
+	return true, nil
+}
